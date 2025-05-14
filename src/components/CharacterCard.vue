a96b7e5b9ca64b18c4a2a7123cc89c7e25bddbc9
Leonardo Nicola <leonardonicolares@gmail.com>
2025-05-14 20:28:33 -0300

    feat: create reusable character card component

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+  character: {
+    id?: string | null;
+    name?: string | null;
+    image?: string | null;
+    status?: string | null;
+    species?: string | null;
+  };
+}>();
+</script>
+<template>
+  <q-card
+    class="character-card cursor-pointer"
+    flat
+    bordered
+    style="width: 200px; height: 320px; border-radius: 15px"
+  >
+    <q-img
+      :src="character.image ?? ''"
+      no-spinner
+      :alt="character.name ?? 'Rick And Morty Character'"
+      ratio="1"
+    />
+
+    <q-card-section class="text-center">
+      <div class="text-subtitle1 text-weight-bold">{{ character.name }}</div>
+      <div class="text-caption">{{ character.species }} - {{ character.status }}</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<style scoped>
+.character-card {
+  transition: transform 0.2s ease;
+}
+
+.character-card:hover {
+  transform: scale(1.05);
+}
+</style>
