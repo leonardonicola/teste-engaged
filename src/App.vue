540e1476bef15ef4d3a44b4f89fcfc4edd6d3b32
Leonardo Nicola <leonardonicolares@gmail.com>
2025-05-14 20:27:11 -0300

    feat: provide urql and add metadata

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import { Client, provideClient, cacheExchange, fetchExchange } from '@urql/vue';
+import { useMeta } from 'quasar';
+
+const client = new Client({
+  url: 'https://rickandmortyapi.com/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
+
+provideClient(client);
+
+useMeta({
+  title: 'EngagED Técnico',
+});
+</script>
+
 <template>
   <router-view />
 </template>
-
-<script setup lang="ts">
-//
-</script>
