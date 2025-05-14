d306a4a549de321e602651930e2258fd3dee49cf
Leonardo Nicola <leonardonicolares@gmail.com>
2025-05-14 20:27:24 -0300

    feat(route): define character routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,14 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      {
+        path: '/personagem/:id',
+        name: 'character',
+        component: () => import('pages/CharacterPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
