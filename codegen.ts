22c819593fd721158ad8b3821ecccb0a79136e37
Leonardo Nicola <leonardonicolares@gmail.com>
2025-05-15 13:28:19 -0300

    refactor(codegen): use env variables for api url

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'https://rickandmortyapi.com/graphql',
+  schema: process.env.API_URL ?? 'http://localhost:8000',
   documents: ['src/queries/**/*.ts'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
